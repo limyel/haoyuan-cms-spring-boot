2be9fb860f2a3b54c1e856a407a9e84c4ad04d46
limyel <limyel@outlook.com>
2022-07-09 19:14:13 +0800

    init

---
+++ src/main/java/com/limyel/haoyuancms/HaoyuanCmsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.limyel.haoyuancms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoyuanCmsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoyuanCmsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limyel/haoyuancms/HaoyuanCmsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limyel.haoyuancms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaoyuanCmsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
